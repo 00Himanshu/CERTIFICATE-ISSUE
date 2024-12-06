050f0f5b614fc1fc5023fb2bdd116928581a714f
Himanshu <thehimanshu94@gmail.com>
2024-12-06 13:43:53 -0500

    updated the sepolia api

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,10 +10,10 @@ module.exports = {
     hardhat: {
       // See its defaults
     },
-    // sepolia: {
-    //   url: "https://eth-sepolia.g.alchemy.com/v2/lHsk508FvXfDO0wO8DGxkumcQR_L_NhA",
-    //   accounts: ["private_key"] //0.01 Eth
-    // }
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/Ll1zMc8AycDiNewQi_xgEGCBlFn02EWO",
+      accounts: ["a11c500f0deae70ba197b24925bc531563209b5f35efb922ac71433941c6cf8a"]
+    }
   },
   solidity: {
     version: "0.8.20",
